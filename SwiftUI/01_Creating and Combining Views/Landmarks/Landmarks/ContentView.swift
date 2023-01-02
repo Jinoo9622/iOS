a8ed485f25b5df1b1e31bd7c26262affe1f2f75f
Jinoo9622 <46417892+Jinoo9622@users.noreply.github.com>
2023-01-02 14:33:45 +0900

    Add files via upload

---
+++ SwiftUI/01_Creating and Combining Views/Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by A on 2022/12/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                // StackView에 modifier 적용 시, StackView 내 component들에 일괄적용
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+
+                Divider()
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+            }
+            .padding()
+
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
